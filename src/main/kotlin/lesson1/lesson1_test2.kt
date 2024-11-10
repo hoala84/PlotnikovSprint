9801b8605c62f7e966d62d2f290f437576abceb5
hoala <hoala84@yandex.ru>
2024-11-10 11:13:49 +0300

    PS-1-2 add worker

---
+++ src/main/kotlin/lesson1/lesson1_test2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+fun main(){
+    val numbersOfOrders: Int = 75
+    val message: String = "Спасибо за покупку. Хорошего дня!"
+    var numbersOfWorker: Int = 2000
+
+    println(numbersOfOrders)
+    println(message)
+   // println(numbersOfWorker)
+
+    numbersOfWorker = 1999
+
+    println(numbersOfWorker)
+}
