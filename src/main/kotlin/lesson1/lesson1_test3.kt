0ff08ad077fb4ede086c864f13a65a644f803b05
hoala <hoala84@yandex.ru>
2024-11-10 12:05:40 +0300

    PS-1-3 create task cosmos

---
+++ src/main/kotlin/lesson1/lesson1_test3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
+
+}
