04fde4ab8f00bff0b85c3acf08886d328ba109f7
hoala <hoala84@yandex.ru>
2024-11-13 07:39:29 +0300

    KS-3-4 create task

---
+++ src/main/kotlin/lesson3/lesson3_test4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var motion = 1
+
+
+    var motionString = "[$from-$to;$motion]"
+    println(motionString)
+
+
+    from = "D2"
+    to = "D3"
+    motion = 2
+
+    motionString = "[$from-$to;$motion]"
+    println(motionString)
+}
