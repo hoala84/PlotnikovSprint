7fe9abdea96954fa0f21e1a81cf606f2c66a2087
hoala <hoala84@yandex.ru>
2024-11-13 22:11:43 +0300

    KS-3-1 внес изменения

--- src/main/kotlin/lesson3/lesson3_test1.kt
+++ src/main/kotlin/lesson3/lesson3_test1.kt
@@ -2,15 +2,16 @@ package org.example.lesson3
 
 fun main() {
 
-    val name = "Иван!"
-    val greetDay = "Добрый день, "
-    val greetEvening = "Добрый вечер, "
+    val name = "Иван"
+    val greetDay = "Добрый день"
+    val greetEvening = "Добрый вечер"
+    val space = ' '
+    val comma = ','
+    val exclam = '!'
 
-    var greeting = greetDay + name
+    var greeting = greetDay + comma + space + name + exclam
     println(greeting)
 
-    greeting = greetEvening + name
+    greeting = greetEvening + comma + space + name + exclam
     println(greeting)
-
-
 }
