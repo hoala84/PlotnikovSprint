569c49a234817f6b049f39b5d3e76210000fe297
hoala <hoala84@yandex.ru>
2024-11-13 07:27:45 +0300

    KS-3-1 create greeting

---
+++ src/main/kotlin/lesson3/lesson3_test1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Иван!"
+    val greetDay = "Добрый день, "
+    val greetEvening = "Добрый вечер, "
+
+    var greeting = greetDay + name
+    println(greeting)
+
+    greeting = greetEvening + name
+    println(greeting)
+
+
+}
