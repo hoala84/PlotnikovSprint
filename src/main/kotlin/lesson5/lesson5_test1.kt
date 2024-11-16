138c3a42106dc40886fd13f96b4dc01226551ed8
hoala <hoala84@yandex.ru>
2024-11-16 08:28:46 +0300

    KS-5-1 сделал диапазон

--- src/main/kotlin/lesson5/lesson5_test1.kt
+++ src/main/kotlin/lesson5/lesson5_test1.kt
@@ -1,12 +1,10 @@
 package org.example.lesson5
 
-import kotlin.random.Random
-
-
 fun main() {
 
-    val number1 = Random.nextInt(0, 100)
-    val number2 = Random.nextInt(0, 100)
+    val range = 1..100
+    val number1 = range.random()
+    val number2 = range.random()
 
     println("Введите сумму этих чисел: $number1 и $number2")
     val userInput = readln().toInt()
