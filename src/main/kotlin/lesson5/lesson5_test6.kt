1699de502a049eb48b0c46219e0a8dde0196e2da
hoala <hoala84@yandex.ru>
2024-11-17 11:32:23 +0300

    KS-5-6 create task

---
+++ src/main/kotlin/lesson5/lesson5_test6.kt
@@ -0,0 +1,27 @@
+package org.example.lesson5
+
+const val CMINM = 100
+
+fun main() {
+
+    println("Введите ваш вес (в килограммах):")
+    val weight = readln().toDouble()
+
+    println("Введите ваш рост (в сантиметрах):")
+    val heightCm = readln().toDouble()
+
+    val heightInM = heightCm / CMINM
+
+    val imt = weight / (heightInM * heightInM)
+
+    val category = when {
+        imt < 18.5 -> "Недостаточная масса тела"
+        imt >= 18.5 && imt < 25 -> "Нормальная масса тела"
+        imt >= 25 && imt < 30 -> "Избыточная масса тела"
+        else -> "Ожирение"
+    }
+
+    println("Ваш ИМТ: %.2f".format(imt))
+    println("Категория: $category")
+}
+
